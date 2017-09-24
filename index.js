2851788c583327a6eb6c78ea4e4f9649e3a45790
Gavin Cooper <gav.c.cooper@outlook.com>
2017-09-24 18:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,12 @@ function sayHiToGrandma(str) {
   if (str === str.toLowerCase()) {
     return "I can't hear you!";
   }
+  else if (str === str.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else if (str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
+
+

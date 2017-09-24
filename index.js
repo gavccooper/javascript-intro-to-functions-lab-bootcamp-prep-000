e7d3202732f9e0bd7dcdf7a66038d8cf247f81d0
Gavin Cooper <gav.c.cooper@outlook.com>
2017-09-24 18:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(str) {
 function logWhisper(str) {
   console.log(str.toLowerCase());
 }
+
+function sayHiToGrandma(str) {
+  if (str === str.toLowerCase()) {
+    return "I can't hear you!";
+  }
+}
